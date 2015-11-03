18cf4d1f09d95d316fd3eb3d5e0e6972e2548848
Gihun Ham <progh2@gmail.com>
2015-11-03 19:27:50 +0900

    반복문 시작 #3

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-echo "Hello World?, 안뇽 연재";
+echo "Hello World?, 안뇽 연재<br />";
+echo "<div style='color:red'>하이루~</div>";
+
+for( $i = 0; $i < 100; $i ++ ){
+   echo "아하하<br>";
+   ?>
+   우하하<?=$i?><br>
+   <?php
+}
+
 phpinfo();
 ?>
